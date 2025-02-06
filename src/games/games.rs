0e85d5bb24c76d246838aa65ea5c0cbdb7d5df98
Michael Tutor <tutor.michael78@gmail.com>
2025-02-06 16:20:14 -0600

    added difficulty choice to guessing game

--- src/games/games.rs
+++ src/games/games.rs
@@ -1,25 +1,25 @@
-use inquire::{CustomType, Confirm};
+use inquire::{CustomType, Confirm, Select};
 use rand::{Rng};
 use std::cmp::Ordering;
 use crate::menus;
 
 pub fn guessing_game() {
     loop {
-        let secret_number = rand::rng().random_range(1..=100);
+        let difficulty_options = vec!["Easy", "Normal", "Hard"];
 
-        println!("Alright I'm thinking of a number between 1 and 100");
+        let difficulty_select = Select::new("Choose your difficulty!", difficulty_options)
+            .prompt()
+            .unwrap()
+            .to_string();
+
+        let secret_number = difficulty(difficulty_select);
 
         loop {
             let player_guess = CustomType::<i32>::new("Try to guess: ")
                 .with_error_message("Please pick a valid number!")
                 .prompt();
 
-            let player_num = player_guess.unwrap();
-
-            // println!("{}", player_num);
-            // println!("{}", secret_number);
-
-            match player_num.cmp(&secret_number) {
+            match player_guess.unwrap().cmp(&secret_number) {
                 Ordering::Greater => println!("That number is too big!"),
                 Ordering::Less => println!("That number is too small!"),
                 Ordering::Equal => {
@@ -38,4 +38,17 @@ pub fn guessing_game() {
     }
 
     menus::main_menu();
+}
+
+fn difficulty(difficulty: String) -> i32 {
+    if difficulty == "Easy" {
+        println!("Alright I am thinking of a number between 1 and 25");
+        rand::rng().random_range(1..=25)
+    } else if difficulty == "Normal" {
+        println!("Alright I am thinking of a number between 1 and 50");
+        rand::rng().random_range(1..=50)
+    } else {
+        println!("Alright I am thinking of a number between 1 and 100");
+        rand::rng().random_range(1..=100)
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,14 @@
 #[path = "utils/utils.rs"] mod utils;
 
 fn main() {
+    // Welcomes the user
     println!("Welcome to the command line game corner!");
     println!("This program is currently under construction using Rust!");
-    // calls the user name function and stores it as a variable
+
+    // Sets the username variable to be passed onto something later
     let player = utils::user_name();
     println!("Welcome {}!", player);
 
+    // Calls the main menu
     menus::main_menu();
 }
