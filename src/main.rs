60dc29926a42a5fbd90513a75e92221593a9abde
tutor <tutor.michael78@gmail.com>
2025-02-10 10:21:11 -0600

    Added creation of a data folder and database on startup of app

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,11 @@ mod menus;
 mod utils;
 
 fn main() {
+    // Creates Data folder
+    utils::utils::create_data_dir().expect("Could not create data dir");
+    // Creates db
+    utils::utils::create_db().expect("Could not create database");
+
     // Welcomes the user
     println!("Welcome to the command line game corner!");
     println!("This program is currently under construction using Rust!");
--- src/utils/utils.rs
+++ src/utils/utils.rs
@@ -1,4 +1,15 @@
+use std::fs::create_dir_all;
+use std::path::{ PathBuf };
 use inquire::{Text, Confirm, Select};
+use directories::BaseDirs;
+use rusqlite::{Connection, Result};
+use rusqlite::fallible_iterator::FallibleIterator;
+
+#[derive(Debug)]
+struct User {
+    id: i32,
+    username: String,
+}
 
 pub fn user_name() -> String {
     let player_name = loop {
@@ -32,4 +43,43 @@ pub fn difficulty() -> String {
         .to_string();
 
     difficulty
+}
+
+// The following functions all deal with the db file
+pub fn create_db() -> Result<()> {
+    let path = path_to_db();
+    let conn = Connection::open(path)?;
+
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY, username TEXT NOT NULL)",
+        (),
+    )?;
+
+    Ok(())
+}
+
+fn path_to_db() -> String {
+    let dir_path = path_to_data_dir();
+
+    let db_path = PathBuf::from(dir_path.join("rusty.db3"));
+
+    db_path.to_str().unwrap().to_string()
+}
+
+pub fn create_data_dir() -> Result<()> {
+    let dir_path = path_to_data_dir();
+
+    create_dir_all(dir_path)
+        .expect("Could not create Rusty Games Directory!");
+
+    Ok(())
+}
+
+fn path_to_data_dir() -> PathBuf {
+    let binding = BaseDirs::new().unwrap();
+    let path = binding.data_local_dir();
+
+    let dir_path = path.join("Rusty Games").join("data");
+
+    dir_path
 }
